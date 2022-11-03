46c799ef63bb961df7e853fa9e2ad7e05518b061
Oleg Fedotov <trevor706@gmail.com>
2022-11-03 16:27:32 +0300

    Initial Commit

---
+++ Movie Informer/Movie_InformerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_InformerApp.swift
+//  Movie Informer
+//
+//  Created by Olegio on 03.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Movie_InformerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
